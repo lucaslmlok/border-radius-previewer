c0ab77b98fe517f59369b6348fa92105fb1c8806
Lucas Lo <lucas.lmlok@gmail.com>
2023-01-14 15:11:33 -0500

    update bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Helmet } from "react-helmet";
+import colors from "tailwindcss/colors";
 import BoxPreview from "./components/BoxPreview";
 import CodePreview from "./components/CodePreview";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg min-h-screen text-white">
+    <div className="bg min-h-screen bg-cyan-600 text-white">
+      <Helmet>
+        <meta name="theme-color" content={colors.cyan[600]}></meta>
+      </Helmet>
+
       <div className="container flex flex-col items-center">
         <h1 className="my-6 text-center text-4xl font-black md:text-5xl">
           Border-radius Previewer
